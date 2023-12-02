514b1bb034e6e1ae528dd13eefcc98c2c187ebad
Abdulhamid1606 <abdulhamidturgunov1606@gmail.com>
2023-12-02 19:58:39 +0500

    salomat

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import HeaderVue from './components/Header.vue'
+  import MainVue from './components/Main.vue'
+
+
+</script>
+<template>
+    <div>
+        <header>
+          <HeaderVue/>
+        </header>
+    </div>
+</template>
+<style scoped>
+
+</style>
